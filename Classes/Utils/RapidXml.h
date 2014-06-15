7077c61220c842e9a7bb702b06dbbff77b7c1f38
xdnm <xdnmsjx@163.com>
2014-06-15 23:23:02 +0800

    RapidXml related

---
+++ Classes/Utils/RapidXml.h
@@ -0,0 +1,10 @@
+#ifndef __RAPID_XML__
+#define __RAPID_XML__
+
+#include "Xml/rapidxml.hpp"
+#include "Xml/rapidxml_iterators.hpp"
+#include "Xml/rapidxml_print.hpp"
+#include "Xml/rapidxml_utils.hpp"
+
+using namespace rapidxml;
+#endif
